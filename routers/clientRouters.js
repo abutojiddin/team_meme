756e5d78cc5c2503a0895d132022c84141d9b55a
Gavharoy <125105968+Gavharoy847606@users.noreply.github.com>
2026-01-16 09:25:40 +0500

    serverdagi ozgarishlar olib tashlandi.

--- routers/clientRouters.js
+++ routers/clientRouters.js
@@ -2,10 +2,10 @@ import express from "express";
 import pool from "../config/connection.js";
 const getClient = express.Router();
 
-getClient.get(`client`, async (req, res) => {
+getClient.get(`/`, async (req, res) => {
   const result = await pool.query("select * from client");
   console.log(result.rows);
   res.json(201).json({ client: result.rows });
 });
 
-export default getClient ;
+export default getClient;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
-import getClient from "./routers/clientRouters"
+
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use(`/`, getClient)
 
 app.get('/test', (req, res) => {
     res.json({ message: "ishlavotdi" })
